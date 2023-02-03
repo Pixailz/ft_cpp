2fc56bcc76b22108827c14f8fbedfefd75eb67b3
Pixailz <pixailz1@gmail.com>
2023-02-03 14:57:28 +0100

    to finish

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,58 @@
+
+/*#BEGIN#_________________________>#_|INFO|_#<______________________________##*/
+/*#                                                        ______ _         ##*/
+/*# DETAILS:                                               | ___ (_)        ##*/
+/*#- FILENAME		Fixed.hpp                               | |_/ /___  __   ##*/
+/*#- PROJECT_NAME	None                                   |  __/| \ \/ /   ##*/
+/*#- AUTHOR			Pixailz                                | |   | |>  <    ##*/
+/*#- CREATED		2023−01−29T23:02:00+0100               \_|   |_/_/\_\   ##*/
+/*#                                                                         ##*/
+/*# DESCRIPTION:                                                            ##*/
+/*## None                                                                   ##*/
+/*#                                                                         ##*/
+/*# VERSION:[ALPHA|BETA]_MAJOR.MINOR.PATCH                                  ##*/
+/*#END#___________________________<#_|INFO|_#>______________________________##*/
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+/*#BEGIN#_____________________>#_|DEPENDENCIES|_#<__________________________##*/
+
+// #include <not_a_library>
+// /**
+//  * <object>		object
+//  * <function>	function()
+//  */
+
+/*#END#_______________________<#_|DEPENDENCIES|_#>__________________________##*/
+
+class Fixed
+{
+	private:
+		// VAR(S)
+
+		// OTHER(S) FUNCTION
+
+	public:
+		// CONSTRUCTOR(S)
+		Fixed(void);
+
+		// DESTRUCTOR(S)
+		~Fixed(void);
+
+		// SETTER(S) FUNCTION
+
+		// GETTER(S) FUNCTION
+
+		// OTHER(S) FUNCTION
+};
+
+/*#BEGIN#________________________>#_|TYPEDEF|_#<____________________________##*/
+
+/*#END#__________________________<#_|TYPEDEF|_#>____________________________##*/
+
+/*#BEGIN#_____________________>#_|PROTOTYPE(S)|_#<__________________________##*/
+
+/*#END#_______________________<#_|PROTOTYPE(S)|_#>__________________________##*/
+
+#endif // FIXED_CLASS
