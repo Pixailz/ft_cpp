2f5516a0fe8e146cabea3e5899a0467330a92d32
Pixailz <pixailz1@gmail.com>
2023-05-20 23:44:15 +0200

    :package: CPP04/TEMPLATE - fix some issue in CPP04/ex01 - add TEMPLATE folder to be fast as f*ck

--- TEMPLATE/src/main.cpp
+++ TEMPLATE/src/main.cpp
@@ -12,46 +12,10 @@
 /*# VERSION:[ALPHA|BETA]_MAJOR.MINOR.PATCH                                  ##*/
 /*#END#___________________________<#_|INFO|_#>______________________________##*/
 
-#include <Dog.hpp>
-#include <Cat.hpp>
-#include <WrongCat.hpp>
+#include <iostream>
 
 int	main(void)
 {
-	{
-		Cat cat_basic;
-		{
-			Cat tmp = cat_basic;
-		}
-		Dog dog_basic;
-		{
-			Dog tmp = dog_basic;
-		}
-	}
-	{		// BASE EXEMPLE
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
-
-		delete j;
-		delete i;
-	}
-	{		// WRONG BASE EXEMPLE
-		const WrongAnimal* j = new WrongCat();
-		WrongCat wrong_cat_basic;
-		{
-			WrongCat tmp = wrong_cat_basic;
-		}
-		j->makeSound();
-		delete j;
-	}
-	{		// check if really no leaks
-		const Animal* animals[4] = { new Dog(), new Dog(), new Cat(), new Cat() };
-
-		for ( int i = 0; i < 4; i++ )
-		{
-			animals[i]->makeSound();
-			delete animals[i];
-		}
-	}
+	std::cout << "Hello World" << std::endl;
 	return (0);
 }
